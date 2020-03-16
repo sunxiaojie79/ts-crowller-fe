3569a1369d78022199ee768b9f2ad6821a6a6e76
sunxiaojie79 <297280911@qq.com>
2020-03-16 18:36:35 +0800

    feat:编写登录表单

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,43 @@
 import React from 'react';
+import { Form, Input, Button } from 'antd';
+import { LockOutlined } from '@ant-design/icons';
 
-const App: React.FC = () =>{
+const NormalLoginForm = () => {
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+
   return (
-    <div>hello world</div>
+    <div className="login-page">
+      <Form
+        name="normal_login"
+        className="login-form"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+      >
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: '请输入密码!',
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined className="site-form-item-icon" />}
+            type="password"
+            placeholder="请输入密码"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">登录</Button>
+        </Form.Item>
+      </Form>
+    </div>
   );
-}
+};
 
-export default App;
+export default NormalLoginForm
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'antd/dist/antd.css';
+import './login.css'; 
 
 ReactDOM.render(<App />, document.getElementById('root'));
