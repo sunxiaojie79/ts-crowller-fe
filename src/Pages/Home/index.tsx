f208f39e54281b1e53810dec39f73ba2ccf2af1f
sunxiaojie79 <297280911@qq.com>
2020-03-17 18:59:51 +0800

    feat:数据爬取功能及引入Echarts

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
 import { Button, message } from 'antd';
+import ReactEcharts from 'echarts-for-react';
 import axios from 'axios';
 import './home.css';
 
@@ -38,17 +39,91 @@ class Home extends Component {
     })
   }
 
+  handleGetCrowllerData = () => {
+    axios.get('/api/getData').then(res => {
+      if (res.data?.data) {
+        message.success('获取成功');
+      } else {
+        message.error('获取失败');
+      }
+    })
+  }
+
+  getOption: () => echarts.EChartOption = () => {
+    return {
+      title: {
+          text: '折线图堆叠'
+      },
+      tooltip: {
+          trigger: 'axis'
+      },
+      legend: {
+          data: ['邮件营销', '联盟广告', '视频广告', '直接访问', '搜索引擎']
+      },
+      grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+      },
+      toolbox: {
+          feature: {
+              saveAsImage: {}
+          }
+      },
+      xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      },
+      yAxis: {
+          type: 'value'
+      },
+      series: [
+          {
+              name: '邮件营销',
+              type: 'line',
+              stack: '总量',
+              data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+              name: '联盟广告',
+              type: 'line',
+              stack: '总量',
+              data: [220, 182, 191, 234, 290, 330, 310]
+          },
+          {
+              name: '视频广告',
+              type: 'line',
+              stack: '总量',
+              data: [150, 232, 201, 154, 190, 330, 410]
+          },
+          {
+              name: '直接访问',
+              type: 'line',
+              stack: '总量',
+              data: [320, 332, 301, 334, 390, 330, 320]
+          },
+          {
+              name: '搜索引擎',
+              type: 'line',
+              stack: '总量',
+              data: [820, 932, 901, 934, 1290, 1330, 1320]
+          }
+      ]
+    };  
+  }
   render() {
     const { isLogin, loaded } = this.state;
     if (isLogin) {
       if (loaded) {
         return (
           <div className="home-page">
-            <Button type="primary" style={{ marginLeft: '5px' }}>
-              爬取
-            </Button>
-            <Button type="primary">展示</Button>
-            <Button type="primary" onClick={this.handleLogout}>退出</Button>
+            <div className="button-group">
+              <Button type="primary" style={{ marginRight: '25px' }} onClick={this.handleGetCrowllerData}>爬取</Button>
+              <Button type="primary" onClick={this.handleLogout}>退出</Button>
+            </div>
+            <ReactEcharts option={this.getOption()} />
           </div>
         );
       }
