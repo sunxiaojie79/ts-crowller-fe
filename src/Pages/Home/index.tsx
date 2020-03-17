b8b773aed0f5db609636da094b64ec19ddfe7ceb
sunxiaojie79 <297280911@qq.com>
2020-03-17 18:11:50 +0800

    feat:登陆退出功能完整优化

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,39 +1,60 @@
 import React, { Component } from 'react';
 import { Redirect } from 'react-router-dom';
-import { Button } from 'antd';
+import { Button, message } from 'antd';
 import axios from 'axios';
 import './home.css';
 
 class Home extends Component {
   state = {
-    isLogin: true
+    isLogin: true,
+    loaded: false
   };
 
   componentDidMount() {
     axios.get('/api/isLogin').then(res => {
       if (!res.data?.data) {
         this.setState({
-          isLogin: false
+          isLogin: false,
+          loaded: true
+        });
+      } else {
+        this.setState({
+          loaded: true
         });
       }
     });
   }
 
+  handleLogout = () => {
+    axios.get('/api/logout').then(res => {
+      if (res.data?.data) {
+        this.setState({
+          isLogin: false,
+          loaded: false
+        });
+      } else {
+        message.error('退出失败');
+      }
+    })
+  }
+
   render() {
-    const { isLogin } = this.state;
+    const { isLogin, loaded } = this.state;
     if (isLogin) {
+      if (loaded) {
         return (
           <div className="home-page">
             <Button type="primary" style={{ marginLeft: '5px' }}>
               爬取
             </Button>
             <Button type="primary">展示</Button>
-            <Button type="primary">退出</Button>
+            <Button type="primary" onClick={this.handleLogout}>退出</Button>
           </div>
         );
-    } else {
-      return <Redirect to="/login" />;
+      }
+      return null;
     }
+    return <Redirect to="/login" />;
   }
 }
 
--- src/Pages/Login/index.tsx
+++ src/Pages/Login/index.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import { Form, Input, Button } from 'antd';
+import React, { useState } from 'react';
+import axios from 'axios';
+import qs from 'qs';
+import { Form, Input, Button, message } from 'antd';
 import { LockOutlined } from '@ant-design/icons';
 import './login.css'; 
+import { Redirect } from 'react-router-dom';
 
 const LoginForm : React.FC = () => {
+  const [isLogin, setIsLogin] = useState(false)
   const onFinish = (values: any) => {
     console.log('Received values of form: ', values);
+    axios.post(
+      '/api/login',
+      qs.stringify({password: values.password}),
+      {
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+        }
+      }
+    ).then(res => {
+      console.log(res)
+      if (res.data?.data) {
+        setIsLogin(true);
+      } else {
+        message.error('登录失败');
+      }
+    })
   };
 
   return (
-    <div className="login-page">
+    isLogin ?
+    (<Redirect to="/home"/>) :
+    (<div className="login-page">
       <Form
         name="normal_login"
         className="login-form"
@@ -37,7 +59,7 @@ const LoginForm : React.FC = () => {
           <Button type="primary" htmlType="submit">登录</Button>
         </Form.Item>
       </Form>
-    </div>
+    </div>)
   );
 };
 
